d0aa589706da2f8b8113bd9b1961a80fb1ebb9d7
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2023-10-13 20:21:06 +0300

    PlaceMan

--- mill.go
+++ mill.go
@@ -1,5 +1,7 @@
 package mill
 
+import "errors"
+
 type Player int
 
 const (
@@ -8,14 +10,45 @@ const (
 	PlayerTwo
 )
 
+var (
+	Success     error = nil
+	ErrBadInput error = errors.New("input is invalid")
+)
+
 type Mill struct {
-	board [3][8]int
+	board         [3][8]Player
+	currentPlayer Player
 }
 
 func NewMill() *Mill {
-	return &Mill{}
+	return &Mill{currentPlayer: PlayerOne}
 }
 
-func (m *Mill) PlaceMan(p Player, a, b int) error {
-	return nil
+func (m *Mill) PlaceMan(a, b int) *error {
+	err := m.placeCellUnrestricted(m.currentPlayer, a, b)
+	if err == &Success {
+		m.nextPlayer()
+	}
+
+	return err
+}
+
+func (m *Mill) placeCellUnrestricted(p Player, a, b int) *error {
+	if m.board[a][b] == PlayerNone {
+		m.board[a][b] = p
+		return m.isMill(a, b)
+	}
+	return &ErrBadInput
+}
+
+func (m *Mill) isMill(a, b int) *error {
+	return &Success
+}
+
+func (m *Mill) nextPlayer() {
+	if m.currentPlayer == PlayerOne {
+		m.currentPlayer = PlayerTwo
+	} else {
+		m.currentPlayer = PlayerOne
+	}
 }
--- mill_test.go
+++ mill_test.go
@@ -1,7 +1,27 @@
 package mill
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPlaceMan(t *testing.T) {
+	mill := NewMill()
+	err := mill.PlaceMan(0, 0)
 
+	if err != &Success {
+		t.Fatalf("the table is empty but cannot place man")
+	}
+
+	err = mill.PlaceMan(0, 0)
+	if err != &ErrBadInput {
+		t.Fatalf("placed man on another man")
+	}
+
+	err = mill.PlaceMan(0, 1)
+	if err != &Success {
+		t.Fatalf("failed to place man in empty cell")
+	}
+
+	fmt.Print(mill.board)
 }
