f1dfb92b42935c7f92d577e56ce31fb8e51ea6d5
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2023-10-13 19:50:47 +0300

    basic mill type, constants and testing

--- mill.go
+++ mill.go
@@ -1,1 +1,21 @@
 package mill
+
+type Player int
+
+const (
+	PlayerNone Player = iota
+	PlayerOne
+	PlayerTwo
+)
+
+type Mill struct {
+	board [3][8]int
+}
+
+func NewMill() *Mill {
+	return &Mill{}
+}
+
+func (m *Mill) PlaceMan(p Player, a, b int) error {
+	return nil
+}
--- mill_test.go
+++ mill_test.go
@@ -1,1 +1,7 @@
 package mill
+
+import "testing"
+
+func TestPlaceMan(t *testing.T) {
+
+}
