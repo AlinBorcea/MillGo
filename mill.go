a7616905943f7d223454bc21a0d7058ba2e1c9a2
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2023-11-10 13:15:51 +0200

    moved player functions from mill.go to player.go

--- mill.go
+++ mill.go
@@ -193,38 +193,3 @@ func (m *Mill) isMill(a, b int) bool {
 
 	return false
 }
-
-func (m *Mill) hasMenLeft() bool {
-	if m.currentPlayer == PlayerOne && m.menLeftToPlacePlayerOne > 0 {
-		return true
-	}
-	if m.currentPlayer == PlayerTwo && m.menLeftToPlacePlayerTwo > 0 {
-		return true
-	}
-
-	return false
-}
-
-func (m *Mill) decreaseMenLeft() {
-	if m.currentPlayer == PlayerOne {
-		m.menLeftToPlacePlayerOne--
-	} else {
-		m.menLeftToPlacePlayerTwo--
-	}
-}
-
-func (m *Mill) increaseMenOnBoard() {
-	if m.currentPlayer == PlayerOne {
-		m.menOnBoardPlayerOne++
-	} else {
-		m.menOnBoardPlayerTwo++
-	}
-}
-
-func (m *Mill) decreaseOpponentsMenOnBoard() {
-	if m.currentPlayer == PlayerOne {
-		m.menOnBoardPlayerTwo--
-	} else {
-		m.menOnBoardPlayerOne--
-	}
-}
--- player.go
+++ player.go
@@ -18,3 +18,38 @@ func NewPlayer() Player {
 		menOnBoard:     0,
 	}
 }
+
+func (m *Mill) hasMenLeft() bool {
+	if m.currentPlayer == PlayerOne && m.menLeftToPlacePlayerOne > 0 {
+		return true
+	}
+	if m.currentPlayer == PlayerTwo && m.menLeftToPlacePlayerTwo > 0 {
+		return true
+	}
+
+	return false
+}
+
+func (m *Mill) decreaseMenLeft() {
+	if m.currentPlayer == PlayerOne {
+		m.menLeftToPlacePlayerOne--
+	} else {
+		m.menLeftToPlacePlayerTwo--
+	}
+}
+
+func (m *Mill) increaseMenOnBoard() {
+	if m.currentPlayer == PlayerOne {
+		m.menOnBoardPlayerOne++
+	} else {
+		m.menOnBoardPlayerTwo++
+	}
+}
+
+func (m *Mill) decreaseOpponentsMenOnBoard() {
+	if m.currentPlayer == PlayerOne {
+		m.menOnBoardPlayerTwo--
+	} else {
+		m.menOnBoardPlayerOne--
+	}
+}
