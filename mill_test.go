4e1fcc2873b8fb8b97efb87970e7d41c57e4c781
AlinBorcea <52531291+AlinBorcea@users.noreply.github.com>
2023-11-10 20:14:51 +0200

    mill_test: deleted TestManually

--- mill_test.go
+++ mill_test.go
@@ -1,60 +1,8 @@
 package mill
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
 	"testing"
 )
-
-func TestManually(t *testing.T) {
-	mill := NewMill()
-	err := &Success
-	scanner := bufio.NewScanner(os.Stdin)
-	placementStage := true
-	moveStage := false
-
-	var a, b, c, d int
-	for err != nil {
-		// input
-		fmt.Print("Input: ")
-		if placementStage && readInputAB(scanner, &a, &b) == nil {
-			err = mill.PlaceMan(a, b)
-		} else if moveStage && readInputABCD(scanner, &a, &b, &c, &d) == nil {
-			err = mill.MoveMan(a, b, c, d)
-		}
-
-		fmt.Println(err)
-	}
-}
-
-func readInputAB(scanner *bufio.Scanner, a, b *int) error {
-	scanner.Scan()
-	rawInput := scanner.Text()
-	input := strings.Split(rawInput, " ")
-	if len(input) != 2 {
-		return fmt.Errorf("bad input")
-	}
-	*a, _ = strconv.Atoi(input[0])
-	*b, _ = strconv.Atoi(input[1])
-	return nil
-}
-
-func readInputABCD(scanner *bufio.Scanner, a, b, c, d *int) error {
-	scanner.Scan()
-	rawInput := scanner.Text()
-	input := strings.Split(rawInput, " ")
-	if len(input) != 4 {
-		return fmt.Errorf("bad input")
-	}
-	*a, _ = strconv.Atoi(input[0])
-	*b, _ = strconv.Atoi(input[1])
-	*c, _ = strconv.Atoi(input[2])
-	*d, _ = strconv.Atoi(input[3])
-	return nil
-}
 
 func TestPlaceMan(t *testing.T) {
 	mill := NewMill()
@@ -73,8 +21,6 @@ func TestPlaceMan(t *testing.T) {
 	if err != &Success {
 		t.Fatalf("failed to place man in empty cell")
 	}
-
-	fmt.Print(mill.board)
 }
 
 func TestMoveMan(t *testing.T) {
